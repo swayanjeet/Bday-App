b0a80b4c752b29e2218a73f438fdbd55ed863891
Swayan Jeet Mishra <swayanjeetmshr657@gmail.com>
2016-06-27 18:45:59 +0530

    Bday App, second commit

--- bday_user/admin.py
+++ bday_user/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bday_App_User,Non_User
 
 # Register your models here.
+admin.site.register(Bday_App_User)
+admin.site.register(Non_User)
--- bday_user/models.py
+++ bday_user/models.py
@@ -1,5 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Non_User(models.Model):
+	age = models.PositiveSmallIntegerField()
+	date_of_birth = models.DateField()
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	pic = models.URLField()
+	facebook_id = models.CharField(max_length=200,blank=True)
+	google_plus_id = models.CharField(max_length=200,blank=True)
+	
+class Bday_App_User(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	age = models.PositiveSmallIntegerField()
+	date_of_birth = models.DateField()
+	pic = models.URLField()
+	facebook_id = models.CharField(max_length=200,blank=True)
+	google_plus_id = models.CharField(max_length=200,blank=True)
+	bday_app_friends = models.ManyToManyField("self")
+	non_bday_app_friends = models.ManyToManyField(Non_User)
+	
+'''class Friends(models.Model):
+	# Primary Friend Indicates first added friend
+	Primary_friend =  # Always a bday app user
+	friends = 
+	bday_app_user = models.BooleanField()#'''
